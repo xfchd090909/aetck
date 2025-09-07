4aa6ab087d9815656d65b3c008255cace79e9b15
aeweb <xfchd090909@gmail.com>
2025-09-07 11:58:34 +0800

    Update proxy.js

--- netlify/edge-functions/proxy.js
+++ netlify/edge-functions/proxy.js
@@ -6,7 +6,7 @@ export default async (request, context) => {
     'https://www.loliapi.com',
     'https://nekos.best',
     'https://api.waifu.im',
-    'https://aenews.dpdns.org'
+    'https://image.anosu.top'
   ];
   
   if (!target || !allowedOrigins.some(origin => target.startsWith(origin))) {
